87bcf02590500a277b1e3bacbdf2dd93f9b545bc
Volodymyr Kryvonos <volodymyr.kryvonos@redvike.com>
2018-05-19 16:27:25 +0200

    Added scaling animation for background image.

--- app/views/login/login.js
+++ app/views/login/login.js
@@ -4,3 +4,12 @@ exports.pageLoaded = function (args) {
   page = args.object
   page.actionBarHidden = true
 }
+
+exports.gridLoaded = function (args) {
+  let background = args.object
+
+  background.animate({
+    scale: { x: 1.1, y: 1.1 },
+    duration: 10000
+  })
+}
