4ddef0dc1e1b856ce28b53ea0c79b62e966e59e8
Volodymyr Kryvonos <volodymyr.kryvonos@redvike.com>
2018-05-19 17:41:14 +0200

    Infinite background image animation.

--- app/views/login/login.js
+++ app/views/login/login.js
@@ -4,12 +4,3 @@ exports.pageLoaded = function (args) {
   page = args.object
   page.actionBarHidden = true
 }
-
-exports.gridLoaded = function (args) {
-  let background = args.object
-
-  background.animate({
-    scale: { x: 1.1, y: 1.1 },
-    duration: 10000
-  })
-}
