50ec9516d1400b954010c9f9ae430be1cf97d99a
Volodymyr Kryvonos <volodymyr.kryvonos@redvike.com>
2018-05-21 17:15:20 +0200

    Added basic layout of login form and implemented transition between start view and login form.

--- app/views/login/login.js
+++ app/views/login/login.js
@@ -1,6 +1,35 @@
-let page = null
+const Animation = require('ui/animation').Animation
+
+let pageView = null
+let loginStartView = null
+let loginFormView = null
+let outerLogoView = null
 
 exports.pageLoaded = function (args) {
-  page = args.object
-  page.actionBarHidden = true
+  pageView = args.object
+  loginStartView = pageView.getViewById('loginStart')
+  loginFormView = pageView.getViewById('loginForm')
+  outerLogoView = pageView.getViewById('outerLogo')
+
+  pageView.actionBarHidden = true
+}
+
+exports.showLoginForm = async function (args) {
+  if (!loginFormView) return
+
+  await loginStartView.animate({
+    opacity: 0
+  })
+
+  loginStartView.visibility = 'collapse'
+  loginFormView.visibility = 'visible'
+  outerLogoView.visibility = 'visible'
+
+  const animations = []
+  const duration = 500
+
+  animations.push({ target: loginFormView, opacity: 1, duration })
+  animations.push({ target: outerLogoView, opacity: 1, duration })
+
+  await new Animation(animations, false).play()
 }
